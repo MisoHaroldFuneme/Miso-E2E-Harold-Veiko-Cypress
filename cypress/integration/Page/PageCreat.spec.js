212c3af6e52652309c78246f35ffda5617042d0d
Harold Funeme <h.funeme@uniandes.edu.co>
2021-05-10 19:49:47 -0500

    initial commit

---
+++ cypress/integration/Page/PageCreat.spec.js
@@ -0,0 +1,14 @@
+describe('Testing basic Angular registration', () => {
+    beforeEach(()=>{
+       cy.visit('http://localhost:2370/ghost/#/site')
+        cy.wait(7000)
+        cy.get('button').click()
+    })
+    it('Test form feedback', () => {
+        cy.get('button.btn.btn-primary').click()
+        cy.wait(1000)
+        cy.get('div.invalid-feedback').then(($divs)=>{
+            expect($divs.length).to.equal(4)
+        })
+    })
+  })
